64ac292278b5537a7f60b0445172d43f25b1dcd5
Liam <your.email@example.com>
2023-07-06 15:41:11 +1200

    bux fixes

--- Door Server/startup.lua
+++ Door Server/startup.lua
@@ -2,32 +2,70 @@ rednet.open("back")
 
 os.loadAPI("touchpoint")
 
-local traiding_door_state = false
+term.clear()
 
-local traiding_door_panel_1 = touchpoint.new("monitor_17")
-traiding_door_panel_1:add("Door 1", function ()
-                toggleAll()
-                traiding_door_state = not traiding_door_state
-                rednet.send(19, traiding_door_state)
+local trading_door_state = false
+local iron_door_state = false
+
+local trading_door_panel_1 = touchpoint.new("monitor_17")
+trading_door_panel_1:add("Door 1", function ()
+                toggleTradingDoor()
 end, 1, 1, 7, 5, colors.lime, colors.red)
 
-local traiding_door_panel_2 = touchpoint.new("monitor_18")
-traiding_door_panel_2:add("Door 1", function ()
-                toggleAll()
-                traiding_door_state = not traiding_door_state
-                rednet.send(19, traiding_door_state)
+local trading_door_panel_2 = touchpoint.new("monitor_18")
+trading_door_panel_2:add("Door 1", function ()
+                toggleTradingDoor()
+        end, 1, 1, 7, 5, colors.lime, colors.red)
+
+
+local iron_door_panel_1 = touchpoint.new("monitor_19")
+iron_door_panel_1:add("Door 1", function ()
+                toggleIronDoor()
 end, 1, 1, 7, 5, colors.lime, colors.red)
 
-function toggleAll()
-        traiding_door_panel_1:toggleButton("Door 1")
-        traiding_door_panel_2:toggleButton("Door 1")    
+local iron_door_panel_2 = touchpoint.new("monitor_20")
+iron_door_panel_2:add("Door 1", function ()
+                toggleIronDoor()
+end, 1, 1, 7, 5, colors.lime, colors.red)
+        
+function toggleTradingDoor()
+        trading_door_state = not trading_door_state
+        rednet.send(19, trading_door_state)
+        trading_door_panel_1:toggleButton("Door 1")
+        trading_door_panel_2:toggleButton("Door 1")    
+        renderUi()
+end
+
+function toggleIronDoor()
+        iron_door_state = not iron_door_state
+        rednet.send(23, iron_door_state)
+        iron_door_panel_1:toggleButton("Door 1")
+        iron_door_panel_2:toggleButton("Door 1")    
+        renderUi()
+end
+
+function renderUi()
+        term.clear()
+        term.setCursorPos(1,1)
+        print("Door 1: ",  trading_door_state)
+        print("Door 2: ",  iron_door_state)    
 end
 
 parallel.waitForAll(
     function()
-        traiding_door_panel_1:run()
+        trading_door_panel_1:run()
     end,
     function()
-        traiding_door_panel_2:run()
+        trading_door_panel_2:run()
+    end,
+    function()
+        iron_door_panel_1:run()
+    end,
+    function()
+        iron_door_panel_2:run()
+    end,
+    function ()
+        renderUi()
     end
 )
+
